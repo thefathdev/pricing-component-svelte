06c7c6b364b9e4642a828e37de1053429290c571
Fathul Irfaan Abdillah <abdillahfathu@gmail.com>
2022-09-04 17:30:25 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Header from './lib/Header.svelte'
+  import ChoosePlan from './lib/ChoosePlan.svelte'
+  import PlanOption from './lib/PlanOption.svelte'
+</script>
+
+<main class="container">
+  <Header />
+  <ChoosePlan />
+  <PlanOption />
+</main>
+
+<style>
+</style>
